2403ff5e9b6a9abb42f435b10de8dd7b148b6d9e
mauro m suzuki <maurosuzuki92@gmail.com>
2017-10-25 21:44:59 -0200

    Unity

---
+++ mobileGit/Assets/Scripts/APIClient/ApiClient.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class ApiClient : MonoBehaviour {
+
+	const string baseUrl = "http://localhost:57842/API";
+
+	void Start () {
+		StartCoroutine(GetItems());
+	}
+	
+	IEnumerator GetItems()
+	{
+		UnityWebRequest request = UnityWebRequest.Get(baseUrl + "/Items");
+		yield return request.Send();
+
+		if(request.isNetworkError || request.isHttpError)
+		{
+			Debug.Log(request.error);
+		}
+		else
+		{
+			string response = request.downloadHandler.text;
+			Debug.Log(response);
+			byte[] bytes = request.downloadHandler.data;
+
+			Item[] Items = JsonHelper.getJsonArray<Item>(response);
+
+			foreach(Item i in Items)
+			{
+				ImprimirItem(i);
+			}
+		}
+	}
+	private void ImprimirItem(Item i)
+	{
+		Debug.Log("====== Dados Objeto =====");
+		Debug.Log("Nome: " + i.Nome);
+		Debug.Log("Descrição: " + i.Descricao);
+		Debug.Log("Dano Máximo: " + i.DanoMaximo);
+		Debug.Log("Raridade: " + i.Raridade);
+		Debug.Log("TipoItemID: " + i.TipoItemID);
+	}
+
+}
